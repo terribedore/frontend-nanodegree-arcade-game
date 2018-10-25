ea5aa913248c246b3647806401d186252deb5f95
Terri Bedore <terribedore@gmail.com>
2018-10-25 08:57:21 -0500

    defined 'allEnemies'

--- js/app.js
+++ js/app.js
@@ -29,6 +29,7 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [new Enemy()];
 
 
 
